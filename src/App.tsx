e603b11a6e63f193564c1bf13eba4770a2a8d519
Michael Krämer <info.m.kraemer@gmail.com>
2025-01-01 20:22:25 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { Component } from 'react';
+import './App.css'
+
+function App() {
+  return (
+    <h1>
+      Hello world!
+    </h1>
+  )
+}
+
+export default App
---
+++ src/shared/header/header.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { Component } from 'react';
+
+interface HeaderProps {
+    
+}
+ 
+interface HeaderState {
+    
+}
+ 
+class Header extends React.Component<HeaderProps, HeaderState> {
+    state = { :  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default Header;
