47f9ef7cbb98c1435e4ab56fac75f08bd9349026
Michael Krämer <info.m.kraemer@gmail.com>
2025-01-04 21:55:33 +0100

    fixed instagram-widget - integrated useEffect (view instagram post pop-up)

--- src/pages/widgets/instagram/insta-widget.tsx
+++ src/pages/widgets/instagram/insta-widget.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const InstagramPage = () => {
+  useEffect(() => {
+    // LightWidget-Skript programmgesteuert hinzufügen
+    const script = document.createElement('script');
+    script.src = 'https://cdn.lightwidget.com/widgets/lightwidget.js';
+    script.async = true;
+    document.body.appendChild(script);
+
+    // Aufräumen, falls das Skript entfernt werden muss
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <div>
       <h2>Meine Instagram-Inhalte</h2>
-      <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script>
       <iframe 
         src="https://cdn.lightwidget.com/widgets/c669fa07b7e05b1ebf5fd46a16427076.html" 
         className="lightwidget-widget" 
-        style={{ width: '100%', border: '0', overflow: 'hidden', height: '100vh',  backgroundColor: 'transparent' }}
+        style={{ width: '100%', border: '0', overflow: 'hidden', height: '100vh', backgroundColor: 'transparent' }}
       />
     </div>
   );
